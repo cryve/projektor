de8f1b42c49952d993c5393eae7baafd0cf81d15
cryve <cryve@users.noreply.github.com>
2016-08-05 12:03:50 +0200

    Change skills input to tags based input

--- client/templates/projects/new_project.js
+++ client/templates/projects/new_project.js
@@ -5,6 +5,7 @@ import "./new_project.html" ;
 Template.newProject.onRendered(function() {
   // Initialize bootstrap tagsinput for tags field
   $("#tags-input").tagsinput();
+  $("#skills").tagsinput();
 });
 
 Template.newProject.events({
@@ -29,6 +30,7 @@ Template.newProject.events({
     target.contacts.value = '' ;
     target.deadline.value = '' ;
     $("#tags-input").tagsinput("removeAll");
+    $("#skills").tagsiinput("removeAll");
 
 
   },
