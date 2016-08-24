4edddb121e7d20581ba414f6d657ae2851bba630
cryve <cryve@users.noreply.github.com>
2016-08-24 09:07:06 +0200

    Style project card according wireframe, sample icons for team member

--- client/templates/projects/project_card.js
+++ client/templates/projects/project_card.js
@@ -3,5 +3,9 @@ import { Template } from 'meteor/templating';
 import { Projects } from '../../../lib/collections/projects.js';
 
 import './project_card.html';
+
+Template.projectCard.onRendered(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
   
   
