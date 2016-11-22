27394cad338895fba9ae45d4b9ac6040569ea243
cryve <cryve@users.noreply.github.com>
2016-11-22 15:53:34 +0100

    Recycle old drafts

--- client/templates/application/navigation_bar.js
+++ client/templates/application/navigation_bar.js
@@ -3,8 +3,15 @@ import {ProjectDrafts} from "/lib/collections/project_drafts.js";
 Template.navigationBar.events({
   "click .create-project-btn" (event) {
     if(Meteor.user()) {
-      var id = ProjectDrafts.insert({});
-      Router.go("newProject", {_id: id});
+      // Go to a not finished draft if exists, else go to new draft
+      var lastDraft = ProjectDrafts.findOne({"owner.userId": Meteor.userId()});
+      let draftId;
+      if (lastDraft && lastDraft._id) {
+        draftId = lastDraft._id;
+      } else {
+        draftId = ProjectDrafts.insert({});
+      }
+      Router.go("newProject", {_id: draftId});
     } else {
       Router.go("loginPage");
       console.log("not signed in");
