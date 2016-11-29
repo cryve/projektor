2da34d54dc21229eeedc872b4ea0a12ac196d1d2
adrianabba <adrian.abbassian@haw-hamburg.de>
2016-11-29 13:30:16 +0100

    added edit function for userSkills, show projects for every userProfile, if involved

--- client/templates/projects/edit_profile.js
+++ client/templates/projects/edit_profile.js
@@ -21,3 +21,22 @@ Template.editAboutMe.events({
     Template.instance().editActive.set(false);
   },
 });
+
+Template.editSkills.onCreated(function() {
+  this.editActive = new ReactiveVar(false);
+});
+
+Template.editSkills.helpers({
+  editActive () {
+    return Template.instance().editActive.get();
+  },
+});
+
+Template.editSkills.events({
+  "click .btn-edit-tags" (event) {
+    Template.instance().editActive.set(true);
+  },
+  "click .btn-abort-editing" (event) {
+    Template.instance().editActive.set(false);
+  },
+});
