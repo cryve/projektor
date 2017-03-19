ed7421dcd301c4b05c8ad0c57bb2e314f2a09880
adrianabba <adrian.abbassian@haw-hamburg.de>
2017-03-19 16:41:10 +0100

    mongo-xlsx packges added, new xlsxFiles collection, addFile method added

--- client/templates/projects/course.js
+++ client/templates/projects/course.js
@@ -3,25 +3,40 @@ import { Meteor } from 'meteor/meteor'
 import { XlsFiles } from '/lib/collections/xlsFiles.js';
 import { Template } from 'meteor/templating';
 
+import './course.html';
+
+
+Template.course.onCreated (function courseOnCreated() {
+  Meteor.subscribe('files.xlsFiles.all');
+  this.downloadLink = new ReactiveVar(false);
+});
+
 Template.course.events({
 
   "click #excel-button" (event){
 
-    Meteor.call("excel", function (error) {
-  // identify the error
-      if (error && error.error === "logged-out") {
-        // show a nice error message
-        Session.set("errorMessage", "Please log in to post a comment.");
+   Meteor.call('excel', {
+      test1: '12345',
+      test2: 'This is a todo item.',
+      test3: 'cookies.'
+    }, (err, res) => {
+      if (err) {
+        alert(err);
+      } else {
+        
       }
     });
-    console.log(this);
-
-  },
+    Template.instance().downloadLink.set(true);
+  }
 });
 
+Template.fileLink.onCreated (function fileLinkOnCreated() {
+  Meteor.subscribe('files.xlsFiles.all');
 
-/*Template.course.helpers({
+});
+Template.fileLink.helpers({
   file: function () {
     return XlsFiles.findOne();
   }
-});*/
+  
+});
