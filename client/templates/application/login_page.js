465e3aff47f16a5605d9ac086fcf0104568dc6bc
cryve <cryve@users.noreply.github.com>
2017-03-29 13:23:23 +0200

    Connect login page to LDAP login
    https://github.com/cryve/projektor/issues/41
    
    * Show login page for guests accessing member-only content by using
      Router hook
    * Add LDAP keywords to login form and set LDAP helpers and events

---
+++ client/templates/application/login_page.js
@@ -0,0 +1,16 @@
+import { Template } from 'meteor/templating'
+import './login_page.html';
+
+Template.loginPage.helpers(_.extend(LDAP.formHelpers, {
+  loggingIn() {
+	   return Meteor.loggingIn();
+  },
+}));
+
+Template.loginPage.events({
+  "submit form"(event) {
+    event.preventDefault();
+  },
+});
+
+Template.loginPage.events(LDAP.formEvents);
