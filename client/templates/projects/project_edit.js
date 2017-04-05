cryve/projektor

commit 67a50a76878c18003e24c6fcddb4b6d4bc8cdea8

Enhance edit permissions and leaving member groups * Refactor leave modal for leaving groups in general * Show leave modal for leaving supervisors * Fix permission updating for users belonging to both team   and supervisors

client/templates/projects/project_edit.js
@@ -140,10 +140,11 @@ Template.member.events({
     Template.instance().editActive.set(false);
   },
   "click .show-leave-modal"(event) {
-    Modal.show("leaveTeamModal", {
+    Modal.show("leaveGroupModal", {
       collectionName: this.currentCollection._name,
       docId: this.currentDoc._id,
       docTitle: this.currentDoc.title,
+      group: "team",
       userId: this.userId,
       userRole: this.role,
       isLastEditor: this.currentDoc.editableBy.length <= 1,
@@ -151,13 +152,24 @@ Template.member.events({
   },
 });
 
-Template.leaveTeamModal.events({
+Template.leaveGroupModal.helpers({
+  groupName() {
+    if(this.group == "team") {
+      return "Team";
+    } else if (this.group == "supervisors") {
+      return "Betreuer";
+    }
+    return "Unbekannt";
+  },
+});
+
+Template.leaveGroupModal.events({
   "click #leave"(event) {
     event.preventDefault();
     deleteEditableArrayItem.call({
       collectionName: this.collectionName,
       docId: this.docId,
-      arrayField: "team",
+      arrayField: this.group,
       item: { userId: this.userId, role: this.userRole },
     },(err, res) => {
         if (err) {
@@ -204,6 +216,17 @@ Template.supervisor.events({
         if (err) {
           alert(err);
         }
+    });
+  },
+  "click .show-leave-modal"(event) {
+    Modal.show("leaveGroupModal", {
+      collectionName: this.currentCollection._name,
+      docId: this.currentDoc._id,
+      docTitle: this.currentDoc.title,
+      group: "supervisors",
+      userId: this.userId,
+      userRole: this.role,
+      isLastEditor: this.currentDoc.editableBy.length <= 1,
     });
   },
 });

lib/methods.js
@@ -215,9 +215,14 @@ export const addMemberToDraft = new ValidatedMethod({
       "You cannot edit draft that is not yours");
     }
     Drafts.update(docId, { $push: { team: member } });
-    if(member.isEditor) {
-      Drafts.update(docId, { $addToSet: { editableBy: member.userId }})
-    }
+    updateEditPermissions.call({
+      collectionName: "drafts",
+      docId: docId,
+    },(err, res) => {
+        if (err) {
+          console.log(err);
+        }
+    });
   }
 });
 
@@ -231,9 +236,14 @@ export const addMemberToProject = new ValidatedMethod({
       "You cannot edit project that is not yours");
     }
     Projects.update(docId, { $push: { team: member } });
-    if(member.isEditor) {
-      Projects.update(docId, { $addToSet: { editableBy: member.userId }})
-    }
+    updateEditPermissions.call({
+      collectionName: "projects",
+      docId: docId,
+    },(err, res) => {
+        if (err) {
+          console.log(err);
+        }
+    });
   }
 });
 
@@ -249,7 +259,14 @@ export const addSupervisorToDraft = new ValidatedMethod({
     const user = Meteor.users.findOne(supervisor.userId);
     supervisor.role = user.profile.role + " für " + user.profile.study;
     Drafts.update(docId, { $push: { supervisors: supervisor } });
-    Drafts.update(docId, { $addToSet: { editableBy: supervisor.userId } });
+    updateEditPermissions.call({
+      collectionName: "drafts",
+      docId: docId,
+    },(err, res) => {
+        if (err) {
+          console.log(err);
+        }
+    });
   }
 });
 
@@ -265,7 +282,14 @@ export const addSupervisorToProject = new ValidatedMethod({
     const user = Meteor.users.findOne(supervisor.userId);
     supervisor.role = user.profile.role + " für " + user.profile.study;
     Projects.update(docId, { $push: { supervisors: supervisor } });
-    Projects.update(docId, { $addToSet: { editableBy: supervisor.userId } });
+    updateEditPermissions.call({
+      collectionName: "projects",
+      docId: docId,
+    },(err, res) => {
+        if (err) {
+          console.log(err);
+        }
+    });
   }
 });
 
@@ -297,8 +321,7 @@ export const deleteEditableArrayItem = new ValidatedMethod({
       throw new Meteor.Error(collectionName+".editable.deleteArrayItem.unauthorized",
       "You cannot delete array item in doc that is not yours");
     }
-    if(doc.editableBy && _.contains(doc.editableBy, item.userId)
-      && (doc.editableBy.length <= 1)) {
+    if(doc.editableBy && doc.editableBy.length <= 1) {
         throw new Meteor.Error(collectionName+".editable.deleteArrayItem.lastEditor",
         "You cannot leave project because you are the only member who can edit it");
     }
@@ -1013,7 +1036,6 @@ export const updateEditPermissions = new ValidatedMethod({
   }).validator(),
   run({collectionName, docId}) {
     const doc = Mongo.Collection.get(collectionName).findOne(docId);
-    let editableBy = [doc.owner.userId];
     _.each(doc.team, function(member) {
       if(member.isEditor) {
         editableBy.push(member.userId);
@@ -1022,7 +1044,6 @@ export const updateEditPermissions = new ValidatedMethod({
     _.each(doc.supervisors, function(supervisor) {
       editableBy.push(supervisor.userId);
     });
-    editableBy = _.uniq(editableBy);
     Mongo.Collection.get(collectionName).update(docId, { $set: { editableBy: editableBy } });
   }
 });

client/templates/projects/project_edit.html
@@ -75,25 +75,27 @@
   </li>
 </template>
 
-<template name="leaveTeamModal">
-  <div id="leaveTeamModal" class="modal fade" tabindex="-1" role="dialog">
+<template name="leaveGroupModal">
+  <div id="leaveGroupModal" class="modal fade" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-sm" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">Projekt-Team verlassen</h4>
+          <h4 class="modal-title">{{groupName}} verlassen</h4>
         </div>
         <div class="modal-body">
-          <p>Möchtest du das Team von <strong>{{docTitle}}</strong> wirklich verlassen?</p>
-          <div class="alert alert-danger" role="alert">
-            Du kannst das Projekt nicht verlassen, da du der einzige bist, der es bearbeiten kann.
-            <br>
-            <small>Gib einem anderen Mitglied Bearbeitungsrechte, füge einen Betreuer hinzu oder lösche das Projekt.</small>
-          </div>
+          <p>Möchtest du die Gruppe "{{groupName}}" von <strong>{{docTitle}}</strong> wirklich verlassen?</p>
+          {{#if isLastEditor}}
+            <div class="alert alert-danger" role="alert">
+              Du kannst das Projekt nicht verlassen, da du der einzige bist, der es bearbeiten kann.
+              <br>
+              <small>Gib einem Mitglied Bearbeitungsrechte, füge einen Betreuer hinzu oder lösche das Projekt.</small>
+            </div>
+          {{/if}}
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
-          <button id="leave" type="button" class="btn btn-danger" disabled={{isLastEditor}}>Team verlassen</button>
+          <button id="leave" type="button" class="btn btn-danger" disabled={{isLastEditor}}>{{groupName}} verlassen</button>
         </div>
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
@@ -320,7 +322,11 @@
       <h4 class="media-heading">
         <a href="{{pathFor 'userProfile' _id=this.userId}}">{{getFullUsername this.userId}}</a>
         {{#if isProjectEditableBy currentDoc currentUser._id}}
-          <a href=""><span class="glyphicon glyphicon-minus text-danger btn-delete-supervisor"></span></a>
+          {{#if $eq this.userId currentUser._id }}
+            <a href=""><span class="glyphicon glyphicon-minus text-danger show-leave-modal"></span></a>
+          {{else}}
+            <a href=""><span class="glyphicon glyphicon-minus text-danger btn-delete-supervisor"></span></a>
+          {{/if}}
         {{/if}}
       </h4>
       <p class="text-muted">{{role}}</p>

commit 12e6cd5ba8f8b429204aa20c2b78b38b637c86f2

Update permissions only when neccessary
* Fix errors in updateEditPermissions and deleteEditableArrayItem
  introduced in https://github.com/cryve/projektor/commit/67a50a76878c18003e24c6fcddb4b6d4bc8cdea8
* Remove client-side updateEditPermissions calls

client/templates/projects/project_edit.js
@@ -124,14 +124,6 @@ Template.member.events({
           alert(err);
         }
     });
-    updateEditPermissions.call({
-      collectionName: this.currentCollection._name,
-      docId: this.currentDoc._id,
-    },(err, res) => {
-        if (err) {
-          alert(err);
-        }
-    });
   },
   "click .btn-edit-member" (event) {
     Template.instance().editActive.set(true);
@@ -176,14 +168,6 @@ Template.leaveGroupModal.events({
           alert(err);
         }
     });
-    updateEditPermissions.call({
-      collectionName: this.collectionName,
-      docId: this.docId,
-    },(err, res) => {
-        if (err) {
-          alert(err);
-        }
-    });
     Modal.hide();
   },
 });
@@ -204,14 +188,6 @@ Template.supervisor.events({
       docId: this.currentDoc._id,
       arrayField: "supervisors",
       item: { userId: this.userId, role: this.role },
-    },(err, res) => {
-        if (err) {
-          alert(err);
-        }
-    });
-    updateEditPermissions.call({
-      collectionName: this.currentCollection._name,
-      docId: this.currentDoc._id,
     },(err, res) => {
         if (err) {
           alert(err);

lib/methods.js
@@ -215,14 +215,16 @@ export const addMemberToDraft = new ValidatedMethod({
       "You cannot edit draft that is not yours");
     }
     Drafts.update(docId, { $push: { team: member } });
-    updateEditPermissions.call({
-      collectionName: "drafts",
-      docId: docId,
-    },(err, res) => {
-        if (err) {
-          console.log(err);
-        }
-    });
+    if(member.isEditor) {
+      updateEditPermissions.call({
+        collectionName: "drafts",
+        docId: docId,
+      },(err, res) => {
+          if (err) {
+            console.log(err);
+          }
+      });
+    }
   }
 });
 
@@ -236,14 +238,16 @@ export const addMemberToProject = new ValidatedMethod({
       "You cannot edit project that is not yours");
     }
     Projects.update(docId, { $push: { team: member } });
-    updateEditPermissions.call({
-      collectionName: "projects",
-      docId: docId,
-    },(err, res) => {
-        if (err) {
-          console.log(err);
-        }
-    });
+    if(member.isEditor) {
+      updateEditPermissions.call({
+        collectionName: "projects",
+        docId: docId,
+      },(err, res) => {
+          if (err) {
+            console.log(err);
+          }
+      });
+    }
   }
 });
 
@@ -321,15 +325,27 @@ export const deleteEditableArrayItem = new ValidatedMethod({
       throw new Meteor.Error(collectionName+".editable.deleteArrayItem.unauthorized",
       "You cannot delete array item in doc that is not yours");
     }
-    if(doc.editableBy && doc.editableBy.length <= 1) {
+    if(doc.editableBy && _.contains(doc.editableBy, item.userId)
+      && doc.editableBy.length <= 1) {
         throw new Meteor.Error(collectionName+".editable.deleteArrayItem.lastEditor",
         "You cannot leave project because you are the only member who can edit it");
     }
     let pullObj = {};
     pullObj[arrayField] = item;
     currentCollection.update( docId, { $pull: pullObj });
+    if(doc.editableBy && _.contains(doc.editableBy, item.userId)) {
+      updateEditPermissions.call({
+        "collectionName": collectionName,
+        "docId": docId,
+      },(err, res) => {
+          if (err) {
+            console.log(err);
+          }
+      });
+    }
   }
 });
+
 export const addCourseToProject = new ValidatedMethod({
   name: "projects.addCourse",
   validate: addCourseSchema.validator(),
@@ -846,14 +862,16 @@ export const updateEditableInDraft = new ValidatedMethod({
       }
     }
     Drafts.update({_id: _id}, modifier);
-    updateEditPermissions.call({
-      collectionName: "drafts",
-      docId: _id,
-    },(err, res) => {
-        if (err) {
-          console.log(err);
-        }
-    });
+    if(userIdKey || isEditorKey) {
+      updateEditPermissions.call({
+        collectionName: "drafts",
+        docId: _id,
+      },(err, res) => {
+          if (err) {
+            console.log(err);
+          }
+      });
+    }
   }
 });
 
@@ -915,15 +933,16 @@ export const updateEditableInProject = new ValidatedMethod({
       }
     }
     Projects.update({_id: _id}, modifier);
-    if(userIdKey || isEditorKey)
-    updateEditPermissions.call({
-      collectionName: "projects",
-      docId: _id,
-    },(err, res) => {
-        if (err) {
-          console.log(err);
-        }
-    });
+    if(userIdKey || isEditorKey) {
+      updateEditPermissions.call({
+        collectionName: "projects",
+        docId: _id,
+      },(err, res) => {
+          if (err) {
+            console.log(err);
+          }
+      });
+    }
   }
 });
 
@@ -1036,6 +1055,7 @@ export const updateEditPermissions = new ValidatedMethod({
   }).validator(),
   run({collectionName, docId}) {
     const doc = Mongo.Collection.get(collectionName).findOne(docId);
+    let editableBy = [];
     _.each(doc.team, function(member) {
       if(member.isEditor) {
         editableBy.push(member.userId);
@@ -1044,6 +1064,7 @@ export const updateEditPermissions = new ValidatedMethod({
     _.each(doc.supervisors, function(supervisor) {
       editableBy.push(supervisor.userId);
     });
+    console.log("updated editableBy:", editableBy);
     Mongo.Collection.get(collectionName).update(docId, { $set: { editableBy: editableBy } });
   }
 });