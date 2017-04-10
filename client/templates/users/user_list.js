984155eb155a63747a4bc0fa47269f0646cd1446
adrianabba <adrian.abbassian@haw-hamburg.de>
2017-04-10 19:06:42 +0200

    server testing

--- client/templates/users/user_list.js
+++ client/templates/users/user_list.js
@@ -7,3 +7,40 @@ Template.userList.helpers({
     return Meteor.users.find({});
   }
 });
+
+Template.userList.events({
+
+  'submit .new-tag' (event){
+    event.preventDefault();
+    Template.instance().keyWord.push($('#listExName').val());
+    Template.instance().setSearch.set(false);
+    return $('#listExName').val('');
+
+  },
+  'click #listExAdd' (event){
+    event.preventDefault();
+    Template.instance().keyWord.push($('#listExName').val());
+    Template.instance().setSearch.set(false);
+    return $('#listExName').val('');
+
+  },
+  'click .listExRemove' (event) {
+    Template.instance().setSearch.set(false);
+    return Template.instance().keyWord.remove(this.toString());
+
+  },
+  'click .listRemove' (event) {
+    Template.instance().setSearch.set(false);
+    return Template.instance().keyWord.clear();
+
+  },
+  'change #sortStatus' (event, template){
+    var selectedSort = template.$("#sortStatus").val();
+    console.log(selectedSort);
+    Template.instance().setSort.set(selectedSort);
+  },
+  'change .sorting': (event) => {
+    ProjectsIndex.getComponentMethods()
+      .addProps('sortBy', $(event.target).val())
+  },
+});
