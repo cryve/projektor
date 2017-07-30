7b7a89fddbe885a6b6a6966260a46936b846e71a
cryve <cryve@users.noreply.github.com>
2017-07-30 17:29:01 +0200

    Split zone creation and adding to zone into separate functions
    Part of Projektors modular UI system
    * Add Projektor.modules.create(zone) function
    * Throw error in create function when zone name is already in use
    * Throw error when trying to add to non-zone

--- packages/projektor-lib/lib/ui_modules.js
+++ packages/projektor-lib/lib/ui_modules.js
@@ -1,25 +1,34 @@
+import { Meteor } from 'meteor/meteor';
+import lodash from 'lodash';
+
 Projektor.modules = {};
 
+Projektor.modules.create = (zone) => {
+  if (typeof Projektor.modules[zone] === 'function') {
+    throw new Meteor.Error('Projektor.modules.create.reservedName',
+      `This name is reserved to the ${zone}-function, please choose a different name, e.g. prefix your package name`);
+  }
+
+  if (Array.isArray(Projektor.modules[zone])) {
+    throw new Meteor.Error('Projektor.modules.create.alreadyExists',
+    `Zone "${zone}" already exists, please choose a different name, e.g. prefix your package name`);
+  }
+
+  Projektor.modules[zone] = [];
+};
+
 Projektor.modules.add = (zone, module) => {
-  if(typeof Projektor.modules[zone] === "function") {
-    return false;
+  if(typeof Projektor.modules[zone] === 'function') {
+    throw new Meteor.Error('Projektor.modules.add.reservedName',
+      `This name is reserved to the ${zone}-function and no zone`);
   }
-
-  if (typeof Projektor.modules[zone] === "undefined") {
-    Projektor.modules[zone] = [];
-  }
-
 
   if (Array.isArray(module)) {
-
-    var modules = module; // we're dealing with an Array, so let's add an "s"
-    modules.forEach( function (module) {
-      Projektor.modules[zone].push(module);
+    const modules = module;
+    lodash.forEach(modules, (singleModule) => {
+      Projektor.modules[zone].push(singleModule);
     });
-
   } else {
-
     Projektor.modules[zone].push(module);
-
   }
 };
--- packages/projektor-lib/tests/ui_modules.tests.js
+++ packages/projektor-lib/tests/ui_modules.tests.js
@@ -5,53 +5,80 @@ import ValidationError from 'meteor/mdg:validation-error'
 import { Random } from 'meteor/random';
 import lodash from 'lodash';
 
-describe("Projektor.modules", function() {
+describe('Projektor.modules', function() {
   beforeEach(function() {
     // Reset all existing zones
     lodash.forEach(Projektor.modules, (value, key) => {
       console.log(value, key);
-      if(!lodash.isFunction(value)) {
+      if (!lodash.isFunction(value)) {
         lodash.unset(Projektor.modules, key);
       }
     });
   });
-  it('should have function: add', function() {
-    chai.assert.isFunction(Projektor.modules.add, "has add function");
+
+  it('should have function: create', function() {
+    chai.assert.isFunction(Projektor.modules.create, 'has create function');
   });
-  it('should add module to zone', function() {
-    const zone = "sampleZone";
+
+  it('should have function: add', function() {
+    chai.assert.isFunction(Projektor.modules.add, 'has add function');
+  });
+
+  it('should create zone', function() {
+    const zone = 'sampleZone';
+    Projektor.modules.create(zone);
+    chai.assert.isArray(Projektor.modules[zone], 'zone has been created');
+  });
+
+  it('should add module to existing zone', function() {
+    const zone = 'sampleZone';
+    Projektor.modules[zone] = [];
     const module = {
-      template: "sampleModuleTemplate"
+      template: 'sampleModuleTemplate'
     };
     Projektor.modules.add(zone, module);
-    chai.assert.isArray(Projektor.modules[zone], "zone has been created");
-    chai.assert.deepEqual(Projektor.modules[zone][0], module, "module was added to zone");
+    chai.assert.deepEqual(Projektor.modules[zone][0], module, 'module was added to zone');
   });
+
   it('should add multiple modules to zone', function() {
-    console.log(Projektor.modules);
-    const zone = "sampleZone";
+    const zone = 'sampleZone';
+    Projektor.modules[zone] = [];
+
     const modules = [
-      { template: "firstTemplate" },
-      { template: "secondTemplate" },
+      { template: 'firstTemplate' },
+      { template: 'secondTemplate' },
     ];
     Projektor.modules.add(zone, modules);
-    chai.assert.isArray(Projektor.modules[zone], "zone has been created");
-    chai.assert.deepEqual(Projektor.modules[zone], modules, "module was added to zone");
-  });
-  it('should not overwrite add function', function() {
-    const zone = "add";
-    const module = {
-      template: "sampleModuleTemplate"
-    };
-    Projektor.modules.add(zone, module);
-    console.log(Projektor);
-    chai.assert.isFunction(Projektor.modules.add, "add has not been overwritten");
 
+    chai.assert.deepEqual(Projektor.modules[zone], modules, 'modules were added to zone');
   });
-  it('should deliver modules from a zone', function() {
-    const zone = "sampleZone";
-    const firstModule = {
-      template: "firstTemplate"
-    }
+
+  it('should not overwrite existing zone', function() {
+    const zone = 'sampleZone';
+    Projektor.modules[zone] = [];
+    chai.assert.throws(() => {
+      Projektor.modules.create(zone);
+    }, Meteor.Error, 'Projektor.modules.create.alreadyExists');
   });
+
+  it('should deny creating or adding to "add"/"create"', function() {
+    const zones = ['create', 'add'];
+    const module = {
+      template: 'sampleModuleTemplate'
+    };
+    lodash.forEach(zones, function(zone) {
+      chai.assert.throws(() => {
+        Projektor.modules.create(zone);
+      }, Meteor.Error, 'Projektor.modules.create.reservedName');
+      chai.assert.throws(() => {
+        Projektor.modules.add(zone, module);
+      }, Meteor.Error, 'Projektor.modules.add.reservedName');
+    });
+    chai.assert.isFunction(Projektor.modules.create, 'create has not been overwritten');
+    chai.assert.isFunction(Projektor.modules.add, 'create has not been overwritten');
+  });
+
+  it('should deliver modules from a zone');
+  it('should remove a module from a zone');
+  it('should remove multiple modules from a zone');
 });
