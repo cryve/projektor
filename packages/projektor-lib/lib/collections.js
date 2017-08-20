0aa9ee56436bde185a9308d9d68d0b587fac0d15
cryve <cryve@users.noreply.github.com>
2017-08-20 19:46:55 +0200

    Prohibit overwriting existing fields
    * Throw error when trying to Collection.addField(field) and field.name
      is already in the schema

--- packages/projektor-lib/lib/collections.js
+++ packages/projektor-lib/lib/collections.js
@@ -1,3 +1,5 @@
+import lodash from 'lodash';
+
 /**
 * Add new field to a collection
 * @param {Object} field
@@ -6,6 +8,10 @@
 */
 Mongo.Collection.prototype.addField = function(field) {
   const collection = this;
+  if (lodash.includes(collection.simpleSchema()._schemaKeys, field.name)) {
+    throw new Meteor.Error(`${collection._name}.addField.alreadyExists`,
+      `The field "${field.name}" already exists. Please choose another name, e.g. prefix with package name`);
+  }
   const fieldSchema = {};
 
   fieldSchema[field.name] = field.schema;
--- packages/projektor-lib/lib/collections.tests.js
+++ packages/projektor-lib/lib/collections.tests.js
@@ -4,27 +4,39 @@ import lodash from 'lodash';
 import SimpleSchema from 'simpl-schema';
 
 describe('Projektor Collection API', function() {
-  let TestCollection = {};
+  let TestCollection;
   before(function() {
-    TestCollection = new Mongo.Collection('testCollection');
-    TestCollection.attachSchema(new SimpleSchema({}));
+    TestCollection = new Mongo.Collection(null);
+    TestCollection.attachSchema(new SimpleSchema({
+      existingField: String,
+    }, {replace: true}));
   });
   it('should give every collection addField method', function() {
     assert.isFunction(TestCollection.addField);
   });
   it('should add new field to collection', function() {
     const field = {
-      name: "testField",
+      name: 'testField',
       schema: {
         type: 'String',
         optional: true,
-      }
+      },
     }
     TestCollection.addField(field);
 
     testFieldSchema = new SimpleSchema({
       testField: field.schema,
     });
+    assert.deepEqual(['existingField', 'testField'], TestCollection.simpleSchema()._schemaKeys);
     assert.deepEqual(TestCollection.simpleSchema().pick(field.name)._schema, testFieldSchema._schema);
+  });
+  it('should not add field with existing field name', function() {
+    const existingField = {
+      name: 'existingField',
+      schema: String,
+    };
+    assert.throws(() => {
+      TestCollection.addField(existingField);
+    }, Meteor.Error, 'null.addField.alreadyExists');
   });
 });
